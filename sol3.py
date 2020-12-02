8e77c5b173617b5eab3e96097ae16a8c92eee7ff
Yair Bleiberg <yairbleiberg@gmail.com>
2020-12-02 20:50:07 +0200

    finished expand function

--- sol3.py
+++ sol3.py
@@ -10,12 +10,25 @@ def build_filter(filter_size):
 
 def reduce(im, filter_size):
     # first blur and take every 2nd pixel along x axis
-    filter = np.expand_dims(build_filter(filter_size), axis=0)
-    im = ndimage.filters.convolve(im, filter, mode='reflect')
+    kernel = np.expand_dims(build_filter(filter_size), axis=0)
+    im = ndimage.filters.convolve(im, kernel, mode='mirror')
     im = im[:, ::2]
     # then do it along y axis
-    im = ndimage.filters.convolve(im, filter.transpose, mode='reflect')
+    im = ndimage.filters.convolve(im, kernel.transpose(), mode='mirror')
     im = im[::2, :]
     return im
 
-print(build_filter(5))
+
+def expand(im, filter_size):
+    N, M = im.shape
+    # first do along x axis
+    kernel = np.expand_dims(build_filter(filter_size), axis=0)
+    im = np.insert(im, np.arange(1, M), 0,  axis=1)
+    im = np.append(im, np.zeros((N,1)), axis=1)
+    # twice the kernel in order to maintain same average pixel brightness when expanding
+    im = ndimage.filters.convolve(im, 2*kernel, mode='mirror')
+    # now, along y axis
+    im = np.insert(im, np.arange(1, N), 0, axis=0)
+    im = np.append(im, np.zeros((1, 2*M)), axis=0)
+    im = ndimage.filters.convolve(im, 2*kernel.transpose(), mode='mirror')
+    return im
